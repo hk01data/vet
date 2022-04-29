e5bdc836f6f488ef3fe1a6d735548f20ec54bd49
hk01data <21330351+hk01data@users.noreply.github.com>
2022-04-29 17:08:40 +0800

    2022 Version (Updating js and icon)

---
+++ 2018/src/districts.js
@@ -0,0 +1,215 @@
+district_data = [
+	{
+		"district": "港島",
+		"anchors" : [
+			{
+				"anchor": "wanchai",
+				"display_name": "灣仔",
+				"order": 4,
+				"lat": 22.278105,
+				"lng": 114.171664,
+				"zoom": 18,
+				"mlat": 22.278105, 
+				"mlng": 114.171664,
+				"mzoom": 16
+			},
+			{
+				"anchor": "causewaybay",
+				"display_name": "銅鑼灣",
+				"order": 5,
+				"lat": 22.279309,
+				"lng": 114.180990,
+				"zoom": 15,
+				"mlat": 22.279309,
+				"mlng": 114.180990,
+				"mzoom": 15
+			},
+			{
+				"anchor": "central",
+				"display_name": "中環",
+				"order": 6,
+				"lat": 22.281449,
+				"lng": 114.155466,
+				"zoom": 17,
+				"mlat": 22.281449,
+				"mlng": 114.155466,
+				"mzoom": 16
+			},
+			{
+				"anchor": "sheungwan",
+				"display_name": "上環",
+				"order": 7,
+				"lat": 22.286594,
+				"lng": 114.147614,
+				"zoom": 15,
+				"mlat": 22.285057,
+				"mlng": 114.147261,
+				"mzoom": 15
+   			},
+            {
+                "anchor": "northpoint",
+                "display_name": "北角",
+                "order": 14,
+                "lat": 22.29074,
+                "lng": 114.198421,
+                "zoom": 15,
+                "mlat": 22.29074,
+                "mlng": 114.198421,
+                "mzoom": 15
+            }
+		]
+	},
+	{
+		"district": "九龍",
+		"anchors" : [
+			{
+				"anchor": "mongkok",
+				"display_name": "旺角",
+				"order": 1,
+				"lat": 22.3193,
+				"lng": 114.169454,
+				"zoom": 16,
+				"mlat": 22.31926,
+				"mlng": 114.16932,
+				"mzoom": 16
+			},
+			{
+				"anchor": "princeedward",
+				"display_name": "太子",
+				"order": 2,
+				"lat": 22.324342,
+				"lng": 114.16828,
+				"zoom": "",
+				"mlat": 22.325195,
+				"mlng": 114.168205,
+				"mzoom": 16
+			},
+			{
+				"anchor": "tsimshatsui",
+				"display_name": "尖沙咀",
+				"order": 3,
+				"lat": 22.299668, 
+				"lng": 114.174544,
+				"zoom": "",
+				"mlat": 22.299668,
+				"mlng": 114.174544,
+				"mzoom": 15
+			},
+			{
+				"anchor": "kwuntong",
+				"display_name": "觀塘",
+				"order": 8,
+				"lat": 22.313683,
+				"lng": 114.221671,
+				"zoom": 15,
+				"mlat": 22.313683,
+				"mlng": 114.221671,
+				"mzoom": 15
+			},
+			{
+				"anchor": "shamshuipo",
+				"display_name": "深水埗",
+				"order": 11,
+				"lat": 22.329697,
+				"lng": 114.163623,
+				"zoom": 15,
+				"mlat": 22.335666,
+				"mlng": 114.159903,
+				"mzoom": ""
+			},
+            {
+                "anchor": "hunghum",
+                "display_name": "紅磡",
+                "order": 15,
+                "lat": 22.3052,
+                "lng": 114.188553,
+                "zoom": 15,
+                "mlat": 22.3052,
+                "mlng": 114.188553,
+                "mzoom": 15
+            }
+		]
+	},
+	{
+		"district": "新界",
+		"anchors" : [
+			{
+				"anchor": "tsuenwan",
+				"display_name": "荃灣",
+				"order": 9,
+				"lat": 22.371299,
+				"lng": 114.115029,
+				"zoom": 15,
+				"mlat": 22.371299,
+				"mlng": 114.115029,
+				"mzoom": 15
+			},
+			{
+				"anchor": "tuenmun",
+				"display_name": "屯門",
+				"order": 10,
+				"lat": 22.396884,
+				"lng": 113.975914,
+				"zoom": 14,
+				"mlat": 22.395527,
+				"mlng": 113.973965,
+				"mzoom": 14
+			},
+			{
+				"anchor": "taipo",
+				"display_name": "大埔",
+				"order": 12,
+				"lat": 22.451605,
+				"lng": 114.164757,
+				"zoom": 15,
+				"mlat": 22.451605,
+				"mlng": 114.164757,
+				"mzoom": 15
+			},
+			{
+				"anchor": "shatin",
+				"display_name": "沙田",
+				"order": 13,
+				"lat": 22.384838,
+				"lng": 114.189542,
+				"zoom": 14,
+				"mlat": 22.384838,
+				"mlng": 114.189542,
+				"mzoom": 14
+			},
+            {
+                "anchor": "yuenlong",
+                "display_name": "元朗",
+                "order": 16,
+                "lat": 22.444596,
+                "lng": 114.026879,
+                "zoom": 15,
+                "mlat": 22.444596,
+                "mlng": 114.026879,
+                "mzoom": 15
+            },
+            {
+                "anchor": "saikung",
+                "display_name": "西貢",
+                "order": 17,
+                "lat": 22.381184,
+                "lng": 114.271674,
+                "zoom": 16,
+                "mlat": 22.381184,
+                "mlng": 114.271674,
+                "mzoom": 16
+            },
+            {
+                "anchor": "tseungkwano",
+                "display_name": "將軍澳",
+                "order": 18,
+                "lat": 22.317211,
+                "lng": 114.256880,
+                "zoom": 14,
+                "mlat": 22.317211,
+                "mlng": 114.256880,
+                "mzoom": 15
+            }
+		]
+	}
+]
--- src/trackEvent.js
+++ src/trackEvent.js
@@ -1,3 +1,24 @@
+// Function to get session_id
+function getSessionId() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    if (document.cookie.match(/hk01_session/)) {
+        return (document.cookie.match(/hk01_session=([\w\-]+)/)[1]);
+    } else {
+        var temp_uid = s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
+        var d = new Date();
+        d.setTime(d.getTime() + (30 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = "hk01_session=" + temp_uid + ";" + expires +
+            ";path=/;domain=." + window.location.host.match(/([^\.]+(\.[^\.]+)?)$/)[1];
+        return (temp_uid);
+    }
+}
+
 // Function to get anonymous_id from cookie
 function getAnonymousId() {
     function s4() {
@@ -22,10 +43,10 @@ function getAnonymousId() {
 // Initialize the tracker client
 var myTracker = new trackerClient({
     GA: {
-        trackingId: ["UA-70981149-1", "UA-70981149-36", "UA-125354638-1"] // UA-125354638-1 is the profile for hk01data editorial team
+        trackingId: ["UA-70981149-1", "UA-70981149-36", "UA-125354638-5"] // UA-125354638-5 is the profile for hk01data editorial team
     },
     Piwik: {
-        trackingUrl: "https://track.hk01.com/v1web/piwik.php",  // replace with your piwik tracking url
+        trackingUrl: "https://track.hk01.com/v2/piwik.php",  // replace with your piwik tracking url
         siteId: 6,  // replace with your piwik site ID
         userId: getAnonymousId(), // replace with user ID, should be same as MEMBER_ID/ANONYMOUS_ID
         isSPA: true // if the page is single page application
@@ -33,11 +54,11 @@ var myTracker = new trackerClient({
 }, false);
 
 /* Config the selected article detail */
-const page_path = "/01偵查/232437/01獨家-整體逾半時間-曬太陽-27幅私人體育會全港互動地圖";
-const author = "\u6881\u9038\u98A8, \u6797\u70B3\u5764, \u9673\u4FE1\u7199, \u90B1\u9756\u6C76, \u52DE\u986F\u4EAE, \u6881\u7956\u9952";  // 梁逸風, 林炳坤, 陳信熙, 邱靖汶, 勞顯亮, 梁祖饒
-const channel = "01\u5075\u67E5"; // 01偵查
-const section = "01\u5075\u67E5"; // 01偵查
-const article_id = "232437";
+const page_path = "/寵物/272385/【寵物地圖】";
+const author = "\u95DC\u6893\u5BE7, \u7C21\u6D69\u5FB7, \u6881\u9038\u98A8";  // 關梓寧, 簡浩德, 梁逸風
+const channel = "\u5BF5\u7269"; // 寵物
+const section = "\u5BF5\u7269"; // 寵物
+const article_id = "272385";
 
 function fireArticlePV(url) {
     try {
@@ -50,7 +71,7 @@ function fireArticlePV(url) {
                 2: section,
                 3: channel,
                 5: article_id
-            }, "https://hk01.com/" + page_path, page_path);
+            }, "https://hk01.com/" + page_path + url, page_path + url);
 
         console.log("fire Article PV");
     }
@@ -64,7 +85,7 @@ function fireMapPV(url) {
     try {
         myTracker.disableGA({
             'UA-70981149-1': true, // production
-            'UA-125354638-1': true, // hk01 data
+            'UA-125354638-5': true, // hk01 data
             'UA-70981149-36': false // data team 
         });
 
